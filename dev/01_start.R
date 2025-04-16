a93a93cc86bddb106b373694216229560425fe83
seewe <eebouele@gmail.com>
2025-04-15 21:01:42 -0400

    initial commit

--- dev/01_start.R
+++ dev/01_start.R
@@ -65,7 +65,7 @@ usethis::use_git()
 ## Sets the remote associated with 'name' to 'url'
 usethis::use_git_remote(
   name = "origin",
-  url = "https://github.com/<OWNER>/<REPO>.git"
+  url = "https://github.com/seewe/d4kliturge.git"
 )
 
 # You're now set! ----
